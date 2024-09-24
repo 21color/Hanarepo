2c44f19a4bc483fdae4609db63329ba6915dc024
21color <lehhha0110@gmail.com>
2024-09-24 13:06:26 +0900

    feat: button 수정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,7 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
   parameters: {
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
--- shared/components/src/atoms/Button/Button.stories.tsx
+++ shared/components/src/atoms/Button/Button.stories.tsx
@@ -2,14 +2,27 @@ import { Meta, StoryObj } from "@storybook/react";
 import Button from "./index";
 
 const meta: Meta<typeof Button> = {
+  tags: ["autodocs"],
   component: Button,
   title: "Components/Button",
   argTypes: {
     variant: {
-      control: {
-        type: "select",
-        options: ["primary", "secondary", "critical"],
-      },
+      description: "버튼의 variant를 설정합니다.",
+      options: ["primary", "secondary", "critical"],
+      control: { type: "radio" },
+    },
+    size: {
+      description: "버튼의 크기를 설정합니다.",
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+    title: {
+      description: "버튼의 텍스트를 설정합니다.",
+      control: { type: "text" },
+    },
+    disabled: {
+      description: "버튼의 비활성화 여부를 설정합니다.",
+      control: { type: "boolean" },
     },
   },
 };
@@ -23,5 +36,6 @@ export const Default: Story = {
     title: "Click me",
     disabled: false,
     size: "medium",
+    onClick: () => alert("Button clicked"),
   },
 };
