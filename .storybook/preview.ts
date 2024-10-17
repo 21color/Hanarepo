a4be22c2e3b9016699e922c678eec5233e3ca9b3
21color <lehhha0110@gmail.com>
2024-10-18 00:07:33 +0900

    feat: var 수정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import "@hanarepo/styles";
 
 const preview: Preview = {
   parameters: {
--- shared/token/vars/spacing.ts
+++ shared/token/vars/spacing.ts
@@ -1,10 +1,10 @@
 export const spacing = {
   none: 0,
-  small: "4px",
-  medium: "8px",
-  large: "12px",
-  xlarge: "16px",
-  xxlarge: "20px",
-  xxxlarge: "24px",
-  xxxxlarge: "28px",
+  small: 'var(--hana-space-1)',
+  medium: 'var(--hana-space-2)',
+  large: 'var(--hana-space-3)',
+  xlarge: 'var(--hana-space-4)',
+  xxlarge: 'var(--hana-space-5)',
+  xxxlarge: 'var(--hana-space-6)',
+  xxxxlarge: 'var(--hana-space-7)',
 } as const;
